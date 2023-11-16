743acb1b1e328b6e6adaa477f3c238cd7b8dec72
beymaster777 <130311554+beymaster777@users.noreply.github.com>
2023-11-15 18:36:45 -0800

    Add files via upload

---
+++ chatterroom.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBWLestWizDpNCnsYRVqDBJqNTVs53Dtc8",
+    authDomain: "chatterbot-3d890.firebaseapp.com",
+    databaseURL: "https://chatterbot-3d890-default-rtdb.firebaseio.com",
+    projectId: "chatterbot-3d890",
+    storageBucket: "chatterbot-3d890.appspot.com",
+    messagingSenderId: "943194593842",
+    appId: "1:943194593842:web:a9ed6e046d7d8687f87a2b"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
